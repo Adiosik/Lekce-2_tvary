71b03882fdcd639f668704a15f3f2781545b2daf
Daniel <simansky.dan@gmail.com>
2023-02-14 20:28:37 +0100

    Kreslení domečku a prasete

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,8 +7,68 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
+        nakresliPrase();
+
         //TODO tady bude kód
     }
+
+    private void nakresliPrase() {
+        zofka.turnRight(90);
+
+        nakresliDomecek();
+
+        nakresliNohu();
+
+        zofka.move(200);
+        zofka.turnLeft(45);
+        zofka.penDown();
+
+        nakresliNohu();
+
+        zofka.turnRight(90);
+        zofka.move(200);
+        zofka.turnLeft(45);
+        zofka.penDown();
+        zofka.move(40);
+    }
+
+    private void nakresliNohu() {
+        zofka.move(40);
+        zofka.turnLeft(180);
+        zofka.penUp();
+        zofka.move(40);
+        zofka.turnRight(90);
+        zofka.penDown();
+        zofka.move(40);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(40);
+        zofka.turnLeft(45);
+    }
+
+    private void nakresliDomecek() {
+        nakresliCtverec();
+        nakresliStrechu();
+    }
+
+    private void nakresliStrechu() {
+        zofka.penUp();
+        zofka.move(200);
+        zofka.turnRight(45);
+        zofka.penDown();
+        zofka.move(143);
+        zofka.turnRight(90);
+        zofka.move(143);
+    }
+
+
+    private void nakresliCtverec() {
+        for (int i = 0; i < 4; i++) {
+            zofka.move(200);
+            zofka.turnRight(90);
+        }
+    }
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
